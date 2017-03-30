8b0bf5a13c0c309d5b4aa54c2fe2640ac1fcdcd6
plopezh <plopezh96@gmail.com>
2017-03-30 08:10:30 +0200

    Update livraisonUsine.java

--- src/livraisonUsine.java
+++ src/livraisonUsine.java
@@ -5,13 +5,18 @@ public class livraisonUsine {
 	private Usine usine;
 	private Prestataire prestExt;
 	private int j;
+	private double cout;
 
 	
 public livraisonUsine(Usine u, Prestataire prest ,int j){
 	this.usine=u;
 	this.prestExt=prest;
 	this.j=j;
+	this.cout=0;
 }
+
+
+
 
 
 
@@ -20,10 +25,10 @@ public Client lePlusProche(Camion camion, LinkedList<Client> clients){
 	int i=0;
 	int j=0;
 	Iterator<Client> iter = clients.iterator() ;
+	double distance= camion.getCoord().distance(clients.getFirst().getCoord());
 	
 	while (iter.hasNext()) {
 		Client c = iter.next() ;
-		double distance= camion.getCoord().distance(c.getCoord());
 	
 		if (distance < camion.getCoord().distance(c.getCoord())){
 			distance=camion.getCoord().distance(c.getCoord());
@@ -36,24 +41,26 @@ public Client lePlusProche(Camion camion, LinkedList<Client> clients){
 
 
 
-public void livraisonPrest(Prestataire p, LinkedList<Client> clients, double quantite){
+public void livraisonPrest(Prestataire p, double quantite){
+	LinkedList<Client> clients = this.usine.getClients();
 	Iterator<Client> iter = clients.iterator();
-	while(iter.hasNext() && quantite>=0){
+	while(iter.hasNext() && quantite>=0) {
 		Client c = iter.next();
-		p.prestDelivre(c,usine, c.getMarchandisesPrest(quantite,j),j);
-		quantite=quantite-c.getMarchandisesPrest(quantite,j);
+		if (c.getMarchandise(this.j)>0) {
+			p.prestDelivre(c,usine, getMarchandises2(quantite,c.getDemandeRestante(j)),j);
+			quantite=quantite-getMarchandises2(quantite,c.getDemandeRestante(j));
+		}
 	}
 	}
 	
 
 
 
-
-public double getMarchandises(Camion c, Client cust){
-	if (c.getCapaUtilisee()<= cust.getDemandeRestante(this.j)){
-		return c.getCapaUtilisee();
+public double getMarchandises2(double q1, double q2){
+	if (q1<= q2){
+		return q1;
 	} else{
-		return cust.getDemandeRestante(this.j);
+		return q2;
 	}
 }
 
@@ -63,35 +70,24 @@ public double getMarchandises(Camion c, Client cust){
 
 
 
-public void livraisonCam(Camion c, LinkedList<Client> clients){
+public void livraisonCam(Camion c){
+	LinkedList<Client> clients= this.usine.getClients();
 	Client clientProche= lePlusProche(c, clients);
-	while(c.peutRetourner(clientProche, this.j) && c.getCapaUtilisee()>0){
-		c.delivre(clientProche,getMarchandises(c,clientProche),this.j);
-		clientDelivre(clients,clientProche);
+	while(c.peutRetourner(clientProche, this.j) && c.getCapaUtilisee()>0 && clientProche.getMarchandise(this.j)>0 ){
+		c.delivre(clientProche,getMarchandises2(c.getCapaUtilisee(), clientProche.getMarchandise(this.j)),this.j);
 		clientProche=lePlusProche(c, clients);
 	}
 	c.retourUsine();
 }
 
 
-public void clientDelivre(LinkedList<Client> listeClients,Client c){//enlÃ¨ve le client de notre liste Ã  livrer
-	Iterator<Client> iter = listeClients.iterator();
-	int i=0;
-	while (iter.hasNext()){
-		Client clientSuivant = iter.next();
-		if (clientSuivant == c && c.getDemandeRestante(j)==0){
-			listeClients.remove(i);
-		}
-		i=i++;
-	}
-}
+
 
 
 
 public void livraisonprodUsine(){
 	
 	Iterator<Camion> iter= this.usine.getCamions().iterator();
-	LinkedList<Client> clients= this.usine.getClients();
 	double quantitePrestataire=this.usine.getQuantiteALivrer(this.j);
 	
 	while(iter.hasNext() && quantitePrestataire>=0){
@@ -104,18 +100,23 @@ public void livraisonprodUsine(){
 	while (iter2.hasNext()){
 		Camion c = iter2.next();
 		if( c.getCapaUtilisee()>0){
-			livraisonCam (c,clients);
+			livraisonCam (c);
 			quantitePrestataire=quantitePrestataire+c.getCapaUtilisee();
 		}
 	}
 	
-	if (quantitePrestataire!=0){// && quantiteLivree>=getMarchandiseALivrer(j) (condition de rÃ©alisabilitÃ©)
-			livraisonPrest(prestExt, clients, quantitePrestataire);
+	if (quantitePrestataire!=0){// && quantiteLivree>=getMarchandiseALivrer(j) (condition de rÃƒÂ©alisabilitÃƒÂ©)
+			livraisonPrest(prestExt, quantitePrestataire);
 		}
 			
-			
+	this.cout= this.cout +  this.prestExt.getCout();
+	
 			
 	}
+
+public double getCout(){
+	return  this.cout;
+}
 
 
 }
