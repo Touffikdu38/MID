7b61dc64d28288a2ddc78e0cb1d0026941743cda
plopezh <plopezh96@gmail.com>
2017-03-30 08:08:17 +0200

    Update Probleme.java

--- src/Probleme.java
+++ src/Probleme.java
@@ -1,41 +1,47 @@
 
+
 public class Probleme {
 	
 	
-	private String nomfichier;
-	private Usine[] tabUsines;
-	private Client[] tabClients;
-	private Camion[] tabCamion;
-	private Prestataire prestataire;
-	private int duree;
+	private Donnees donnee;
+	private Affectation affect;
+	private Production prod;
+	private livraison liv;
 
 
 	public Probleme (String nomfichier){
-		LireDonnees(nomfichier);	
+		this.donnee = new Donnees(nomfichier);	
+		this.affect = new Affectation(this.donnee.getUsines(), this.donnee.getClients(), this.donnee.getCamions());
+		affect.plClient();
+		affect.associeCamions();
+		this.prod = new Production(this.affect,this.donnee.getDuree());
+		this.prod.plProd();
+		this.prod.plStock();
+		this.prod.setVentes();
+		this.liv =new livraison(this.prod.getUsines(),this.donnee.getPrestataire());
+		liv.resolutionLivraison();
 	}
 	
 
-	private void LireDonnees(String nomfichier) {
-		Donnees donnees = new Donnees(nomfichier);
-		donnees.lire();
-		tabUsines=donnees.getUsines();
-		tabClients=donnees.getClients();
-		prestataire = donnees.getPrestataire();
-		tabCamion = donnees.getCamions();
-		duree=donnees.getDuree();
-	}
-
-	Affectation affectation = new Affectation(this.tabUsines, this.tabClients);
-	Production production = new Production(this.tabUsines,this.tabClients,this.duree);
-	livraison livraison =new livraison(this.tabUsines,this.prestataire);
-	
 	public double getCout(){
-		double coutProd= production.getCoutProdStock();
-		double coutLog = livraison.getCoutLogistique();
+		double coutProd= this.prod.getCoutProdStock();
+		double coutLog = this.liv.getCoutLogistique();
 		return coutProd+coutLog;
 	}
 	
 	
+	
+	public livraison getLiv(){
+		return this.liv;
+	}
+	
+	public Affectation getAffect(){
+		return this.affect;
+	}
+	
+	public Production getProd(){
+		return this.prod;
+	}
 	
 	
 
