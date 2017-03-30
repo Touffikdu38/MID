d14faf8f18d29d7660bd7997d355087f21a05cc2
plopezh <plopezh96@gmail.com>
2017-03-30 08:10:05 +0200

    Update livraison.java

--- src/livraison.java
+++ src/livraison.java
@@ -1,29 +1,26 @@
-iimport java.util.Iterator;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class livraison {
-	private LinkedList<Usine> usines;
+	private Usine[] usines;
 	private Prestataire p;
-	private LinkedList<Camion> listeDeCamions;
 	private double cout;
 	
+	
 
-public livraison(LinkedList<Usine> usines, Prestataire p){
+public livraison(Usine[] usines, Prestataire p){
 	this.usines = usines;
 	this.p=p;
 	this.cout=0;
 	
 }
-public void affectationCamions(){
-	
-}
-
 
 public void resolutionLivraison(){
-	for (int j=0; j<7;j++){
-		Iterator<Usine> iter= this.usines.iterator();
-		while (iter.hasNext()){
-			Usine u=iter.next();
+	int nbUsines=this.usines.length;
+	int nbJour=this.usines[0].getJour();
+	for (int j=0; j<nbJour;j++){
+		for (int i=0;i<nbUsines;i++ ){
+			Usine u = this.usines[i];
 			livraisonUsine lu = new livraisonUsine(u,p,j);
 			lu.livraisonprodUsine();
 			cout+=lu.getCout();
@@ -47,16 +44,27 @@ public double getCoutLogistique(){
 }
 
 public double coutPenalite() {
-	Iterator<Usine> iter = this.usines.iterator();
-	Usine usine = iter.next();
+	int nbUsines=this.usines.length;
 	double coutPenalite=0;
-	while(iter.hasNext()){
-		Iterator<Client> iter1= usine.getClients().iterator();
+	for (int i=0;i<nbUsines;i++){
+		Iterator<Client> iter1= this.usines[i].getClients().iterator();
 		Client client = iter1.next();
 		coutPenalite+=client.coutPenalite();
 
-	}
+		}
+	
 	return coutPenalite;
 	
 }
+
+
+public Usine[] getUsine(){
+	return this.usines;
+}
+
+public Prestataire getPrestataire(){
+	return this.p;
+	
+}
+
 }
