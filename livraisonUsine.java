a7e35d4c6cb6a04f4ab62e8945bddba286b05274
Touffikdu38 <lagrange.laurent@hotmail.fr>
2017-03-31 08:45:30 +0200

    Update livraisonUsine.java

--- livraisonUsine.java
+++ livraisonUsine.java
@@ -6,125 +6,76 @@ public class livraisonUsine {
 	private Prestataire prestExt;
 	private int j;
 	private double cout;
-	private LinkedList<Camion> listeCamions;
 
+	/**
+	 * Constructeur d'une livraison usine
+	 * @param u objet de type Usine représentant une usine
+	 * @param prest objet de type Prestataire représentant un prestataire
+	 * @param j entier représentant le jour
+	 */
 	
-public livraisonUsine(Usine u, Prestataire prest ,int j){
-	this.usine=u;
-	this.prestExt=prest;
-	this.j=j;
-	this.cout=0;
-}
+	public livraisonUsine(Usine u, Prestataire prest ,int j){
+		this.usine=u;
+		this.prestExt=prest;
+		this.j=j;
+		this.cout=0;
+	}
 
+	/**
+	 * Méthode qui livre la quantité restante à tout les clients d'une usine grâce au prestataire
+	 * @param p
+	 * @param quantite
+	 */
 
-
-
-public Client lePlusProche(Camion camion, LinkedList<Client> clients){
-	int i=0;
-	int j=0;
-	Iterator<Client> iter = clients.iterator() ;
-	
-	while (iter.hasNext()) {
-		Client c = iter.next() ;
-		double distance= camion.getCoord().distance(c.getCoord());
-	
-		if (distance < camion.getCoord().distance(c.getCoord())){
-			distance=camion.getCoord().distance(c.getCoord());
-			j=i;
+	public void livraisonPrest(Prestataire p, double quantite){
+		LinkedList<Client> clients = this.usine.getClients();
+		Iterator<Client> iter = clients.iterator();
+		while(iter.hasNext() && quantite>=0) {
+			Client c = iter.next();
+			if (c.getMarchandise(this.j)>0) {
+				p.prestDelivre(c,usine, min(quantite,c.getDemandeRestante(j)),j);
+				quantite=quantite-min(quantite,c.getDemandeRestante(j));
+			}
 		}
-		i++;
-	}
-	return clients.get(j);
-}
-
-
-
-public void livraisonPrest(Prestataire p, LinkedList<Client> clients, double quantite){
-	Iterator<Client> iter = clients.iterator();
-	while(iter.hasNext() && quantite>=0){
-		Client c = iter.next();
-		p.prestDelivre(c,usine, c.getMarchandisesPrest(quantite,j),j);
-		quantite=quantite-c.getMarchandisesPrest(quantite,j);
-	}
-	this.cout= this.cout + p.getCout();
-	}
-	
-
-
-
-
-public double getMarchandises(Camion c, Client cust){
-	if (c.getCapaUtilisee()<= cust.getDemandeRestante(this.j)){
-		return c.getCapaUtilisee();
-	} else{
-		return cust.getDemandeRestante(this.j);
-	}
-}
-
-
-
-
-
-
-
-public void livraisonCam(Camion c, LinkedList<Client> clients){
-	Client clientProche= lePlusProche(c, clients);
-	while(c.peutRetourner(clientProche, this.j) && c.getCapaUtilisee()>0){
-		c.delivre(clientProche,getMarchandises(c,clientProche),this.j);
-		clientDelivre(clients,clientProche);
-		clientProche=lePlusProche(c, clients);
-	}
-	c.retourUsine();
-	this.cout=this.cout+c.getCout();
-}
-
-
-public void clientDelivre(LinkedList<Client> listeClients,Client c){//enlÃƒÂ¨ve le client de notre liste ÃƒÂ  livrer
-	Iterator<Client> iter = listeClients.iterator();
-	int i=0;
-	while (iter.hasNext()){
-		Client clientSuivant = iter.next();
-		if (clientSuivant == c && c.getDemandeRestante(j)==0){
-			listeClients.remove(i);
-		}
-		i=i++;
-	}
-}
-
-
-
-public void livraisonprodUsine(){
-	
-	Iterator<Camion> iter= this.usine.getCamions().iterator();
-	LinkedList<Client> clients= this.usine.getClients();
-	double quantitePrestataire=this.usine.getQuantiteALivrer(this.j);
-	
-	while(iter.hasNext() && quantitePrestataire>=0){
-		Camion c= iter.next();
-		c.addCapacite(quantitePrestataire);
-		quantitePrestataire= quantitePrestataire - c.getCapaUtilisee();
 	}
 
-	Iterator<Camion> iter2= this.usine.getCamions().iterator(); 
-	while (iter2.hasNext()){
-		Camion c = iter2.next();
-		if( c.getCapaUtilisee()>0){
-			livraisonCam (c,clients);
-			quantitePrestataire=quantitePrestataire+c.getCapaUtilisee();
+	/**
+	 * @param q1 quantité 1
+	 * @param q2 quantité 2
+	 * @return le minimum des deux quantités
+	 */
+
+	public double min(double q1, double q2){
+		if (q1<= q2){
+			return q1;
+		} else{
+			return q2;
 		}
 	}
 	
-	if (quantitePrestataire!=0){// && quantiteLivree>=getMarchandiseALivrer(j) (condition de rÃƒÂ©alisabilitÃƒÂ©)
-			livraisonPrest(prestExt, clients, quantitePrestataire);
-		}
-			
-			
-			
+	/**
+	 * Accesseur
+	 * @return le coût du prestataire
+	 */
+	
+	public double getCout(){
+		return  this.cout;
 	}
-
-public double getCout(){
-	return this.cout;
-}
-
+	
+	/**
+	 * Méthode qui livre tout les clients d'une usine grâce au prestataire
+	 */
+	
+	public void livraisonToutPrest(){
+		Iterator<Client> iter=this.usine.getClients().iterator();
+		while(iter.hasNext()){
+			Client client=iter.next();
+			if (client.getMarchandise(j)!=0){
+				this.prestExt.prestDelivre(client, this.usine, client.getMarchandise(j), j);
+				this.cout+= this.prestExt.getCoutTransport(j)*this.usine.getCoord().distance(client.getCoord()) + this.prestExt.getCoutFixe(j);
+			}
+		}
+		
+	}
 
 }
